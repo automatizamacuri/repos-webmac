3e1923225f816b12d94a66f63ea88ebbd50b44fa
webmac <macuri516@gmail.com>
2020-10-12 20:34:12 -0500

    comit 01 desde servidor

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>titulo de macuri</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h2>este es el repositio de hd macrui</h2>
+</body>
+</html>
